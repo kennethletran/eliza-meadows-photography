210d3a2511f98034ae76a0e1f434f975514c7b7d
Kenneth Le-Tran <kenneth.letran@gmail.com>
2021-06-11 14:22:41 -0500

    added favicon to site

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -3346,7 +3346,7 @@ heroImg.addEventListener('mouseover', function () {
 heroImg.addEventListener('mouseleave', function () {
   headline.classList.remove('headline--anim');
 });
-},{"locomotive-scroll":"node_modules/locomotive-scroll/dist/locomotive-scroll.esm.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"locomotive-scroll":"node_modules/locomotive-scroll/dist/locomotive-scroll.esm.js"}],"../../../.nvm/versions/node/v12.18.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -3374,7 +3374,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41031" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46145" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -3550,5 +3550,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
+},{}]},{},["../../../.nvm/versions/node/v12.18.3/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
 //# sourceMappingURL=/app.c328ef1a.js.map
