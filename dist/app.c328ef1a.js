aed8496b85ff2075777ba2e68eb4ec2d2ccf0b05
Kenneth Le-Tran <kenneth.letran@gmail.com>
2021-06-13 19:19:52 -0500

    fixed headline positioning on android viewport

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -3374,7 +3374,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46145" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46093" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
